51bb30f59b6b60a043d83f538436f5a666d4782e
tuxedoar <tuxedoar@gmail.com>
2017-11-18 22:30:46 -0300

    Use argparse instead of getopt. Translate everything to English!.

--- cisco_stuff/switch-cmd-remoto.py
+++ cisco_stuff/switch-cmd-remoto.py
@@ -1,108 +1,123 @@
 #!/usr/bin/env python
 
-# Permite ejecutar comandos de forma remota en switches u otros equipos, utilizando SSH!.
-# It allows to execute several commands in different switches at once, using SSH. 
+"""
+Copyright 2017 by tuxedoar@gmail.com .
+
+LICENSE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+DESCRIPTION
+
+It allows to execute several commands in different managed switches at once,
+using SSH. 
+
+"""
 
 import paramiko
+import argparse
 from time import sleep
-import sys, re, getopt
+import sys
+import re
 import getpass
 from socket import error
 
 switches = []
 user = ''
-archivo = ''
+hosts_file = ''
 cmd = []
 
-def sesionSSH(user, cmd):
 
-  # cmd lo toma como una lista con cada elemento separado, asi que los uno. 
-  cmd = ' '.join(cmd)
+def main():
+  parser = argparse.ArgumentParser(
+      description='Exceute remote commands on several managed switches or servers.')
+  parser.add_argument('-f', '--file', required=True, action='store', 
+                      help='Plain text file with list of hosts')
+  parser.add_argument('-u', '--user', required=True, action='store',
+                      help='User to login on remote hosts')
+  parser.add_argument('-c', '--commands', required=True, action='store',
+                      help='Comma separated commands to be executed on remote hosts')
+  args = parser.parse_args()
+
+  if args.file != None and args.user != None and args.commands != None:
+    user=args.user
+    hosts_file=args.file
+    cmd=args.commands
+
+    ReadFile(hosts_file)
+    SSHsession(user, cmd)
+  else: 
+    parser.print_help()
+
+
+def SSHsession(user, cmd):
+
   cmd = cmd.split(',')
-  pw = getpass.getpass('\n Introduzca clave de acceso a los hosts: ')
+  pw = getpass.getpass('\n Please, enter your password to access hosts: ')
 
-  # Inicio sesion para cada una de las IP en la lista de switches
+  # Start session on each IP address in the switches list.
   for ip in switches:
     try:
-      print "\n Conexion a %s con el usuario %s ... \n" % (ip, user) 
-      sesion = paramiko.SSHClient()
-      sesion.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      sesion.connect(ip, username=user, password=pw, look_for_keys=False, allow_agent=False, timeout=10)
+      print "\n Connecting to %s with the user %s ... \n" % (ip, user) 
+      MySession = paramiko.SSHClient()
+      MySession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      MySession.connect(ip, username=user, password=pw, look_for_keys=False, allow_agent=False, timeout=10)
       timeout_modifier = 1.1  # wait 10% longer than usual
 
-      comando = sesion.invoke_shell()
+      UserCommands = MySession.invoke_shell()
       sleep(2)
       for c in cmd:
         # Si las hay, remuevo comillas dobles!.
         c = c.replace('\"','')
-        comando.send(c+'\n')
-
+        UserCommands.send(c+'\n')
       
-      # No funciona si no le doy 1 seg de retardo para el envio del comando!.
+      # Wait 1 second before sending commands provided by user!.
       sleep(1)
-      # Tamanio del buffer de salida en bytes!.
-      salida = comando.recv(8000)
+      # Ouput buffer size in bytes!.
+      output = UserCommands.recv(8000)
 
-      # Divido la salida 1 x linea y la guardo en una lista
-      salida = salida.splitlines()
-      # Ignoro las primeras 12 lineas (elementos de la lista)!.
-#     salida = salida[12:]
-      for lineas in salida:
-        print lineas
-
+      # Split each line in output and store them in a list.
+      output = output.splitlines()
+      for lines in output:
+        print lines
 
     except paramiko.ssh_exception.AuthenticationException, e:
-            print "Ocurrio un error de autenticacion al conectarse a %s : %s" % (ip, e)
+            print "An authentication error ocurred when trying to connect to %s : %s" % (ip, e)
     except paramiko.SSHException, e:
-            print "Ocurrio un error al conectarse  a %s : %s " % (ip, e)
+            print "An error has ocurred while connecting to %s : %s " % (ip, e)
     except error:
-            print "Expiro el tiempo de conexion!. Revise la conectividad con el host de destino!."          
+            print "Connection timed out!. Check your network connection!."          
+
                                         
-def LeerArchivo(archivo):
-# Leo el archivo, extraigo las IP y las almaceno en una lista.
+def ReadFile(hosts_file):
+# Read the file, extract IP adresses and store them on a list.
   try:
-    lectura = open(archivo, 'r')
-    for linea in lectura.readlines():
-      linea=linea.strip()
-      if linea and not linea.startswith('#'):
-        valida = re.search("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", linea)
-        if not valida:
-          print "ATENCION: Se ignora la IP %s por NO ser valida!" % (linea)
+    file = open(hosts_file, 'r')
+    for line in file.readlines():
+      line=line.strip()
+      if line and not line.startswith('#'):
+        valid_ip = re.search("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", line)
+        if not valid_ip:
+          print "WARNING: The IP %s is ignored for not being valid!" % (line)
         else:
-          ip = linea
+          ip = line
           switches.append(ip)
           
-    lectura.close()
+    file.close()
   except IOError:
-    print "No se puede leer el archivo especificado!."
-    uso()
+    print "Can't read the specified file. Make sure it exist!."
     sys.exit(2)     
-  except:
-    uso()
-    sys.exit(2)
 
-def uso():
-  print """
-  USO: switch-cmd-remoto.py [OPCIONES]
-
-  -u: Nombre de usuario       
-  -f [ARCHIVO]: Lee las IPs de un archivo
-  -c "comando1,comando2,comando3": Comando/s a ejecutar
-  """
-
-try:
-  myopts, args = getopt.getopt(sys.argv[1:],"u:f:c:")
-except:
-  print "Uso incorrecto!"
-  sys.exit(2)
-        
-for o, a in myopts:
-  if o == '-u':
-    user=sys.argv[2]
-  elif o == '-f':
-    archivo=sys.argv[4]
-  elif o == '-c':
-    cmd=sys.argv[6:]
-
-LeerArchivo(archivo)
-sesionSSH(user, cmd)
+if __name__ == "__main__":
+  main()
